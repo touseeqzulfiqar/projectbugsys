bb654f3e8c57a485e08fb046eb7e7333e400f4ee
touseeqzulfiqar <touseeqzulfiqar@gmail.com>
2024-09-03 14:49:42 +0500

    changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,33 +3,30 @@ class ProjectsController < ApplicationController
   load_and_authorize_resource
 
   def index
-  @pagy, @projects = pagy(Project.accessible_by(current_ability))
-  @bugs = Bug.accessible_by(current_ability)
-end
-
+    @pagy, @projects = pagy(Project.accessible_by(current_ability))
+    @bugs = Bug.accessible_by(current_ability)
+  end
 
   def show
     @bugs = @project.bugs
   end
 
   def new
-  @project = Project.new
-  @project.users.build
-  @qa_users = User.QA
-  @developer_users = User.developer
-  @selected_developer_users = [] # Initialize as an empty array
-end
+    @project = Project.new
+    @project.users.build
+    @qa_users = User.QA
+    @developer_users = User.developer
+    @selected_developer_users = [] # Initialize as an empty array
+  end
 
   def edit
-  @qa_users = User.QA
-  @developer_users = User.developer.where.not(id: @project.users.pluck(:id))
-  @selected_developer_users = @project.users.developer
-  @project.users.build unless @project.users.any?
-end
-
+    @qa_users = User.QA
+    @developer_users = User.developer.where.not(id: @project.users.pluck(:id))
+    @selected_developer_users = @project.users.developer
+    @project.users.build unless @project.users.any?
+  end
 
   def create
-    debugger
     @project = Project.new(project_params)
     @project.creator_id = current_user.id
     if @project.save
@@ -81,16 +78,15 @@ end
   end
 
   def assign_developers_to_project
-  return unless params[:project][:developer_ids].present?
+    return unless params[:project][:developer_ids].present?
 
-  # Split the comma-separated developer_ids into an array
-  developer_ids = params[:project][:developer_ids].split(',')
+    # Split the comma-separated developer_ids into an array
+    developer_ids = params[:project][:developer_ids].split(',')
 
-  # Find the developer users by their IDs
-  developer_users = User.where(id: developer_ids)
+    # Find the developer users by their IDs
+    developer_users = User.where(id: developer_ids)
 
-  # Assign the developers to the project
-  @project.users << developer_users
-end
-
+    # Assign the developers to the project
+    @project.users << developer_users
+  end
 end
